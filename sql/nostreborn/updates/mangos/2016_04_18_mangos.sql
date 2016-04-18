27779393c13eb8b2e943fd949b110cc564281a02
Saber-NR <caros17@live.com>
2016-04-18 23:29:47 +0800

    Update 2016_04_18_mangos.sql
    Added flag (Interract_Condition) to game object ID 7923, to make it non-interactable with out the quest.

--- sql/nostreborn/updates/mangos/2016_04_18_mangos.sql
+++ sql/nostreborn/updates/mangos/2016_04_18_mangos.sql
@@ -1,1 +1,2 @@
-UPDATE `creature_template` SET `UnitClass`="2" WHERE `Entry`="6250"
+UPDATE `creature_template` SET `UnitClass`="2" WHERE `Entry`="6250";
+UPDATE `gameobject_template` SET `flags`="4" WHERE `entry`="7923";
