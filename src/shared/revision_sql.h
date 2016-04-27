384f87023cb3a4eff97093dfc92b220f82bdbe15
Denis Yemets <pgsilent@gmail.com>
2016-04-27 23:27:25 +0500

    Update revision_sql.h

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -2,5 +2,5 @@
 #define __REVISION_SQL_H__
  #define REVISION_DB_REALMD "required_z2678_01_realmd"
  #define REVISION_DB_CHARACTERS "required_z2682_01_characters_pvpstats"
- #define REVISION_DB_MANGOS "required_z2683_01_mangos_scriptdev2_tables"
+ #define REVISION_DB_MANGOS "required_z2683_a0004_01_mangos_spell_bonus_data"
 #endif // __REVISION_SQL_H__
