bae875dfb1d42b2a9d51b0def91acb579dfed3fc
sidsukana <pgsilent@gmail.com>
2016-05-05 15:57:39 +0500

    Fix bonus healing for Holy Light and Flash of Light

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -3674,12 +3674,16 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
         }
         case SPELLFAMILY_PALADIN:
         {
+            Unit* caster = GetAffectiveCaster();
+
             // Holy Light
             if (m_spellInfo->SpellIconID == 70)
             {
                 if (!unitTarget || !unitTarget->isAlive())
                     return;
                 int32 heal = damage;
+                heal = caster->SpellHealingBonusDone(unitTarget, m_spellInfo, heal, HEAL);
+                heal = unitTarget->SpellHealingBonusTaken(caster, m_spellInfo, heal, HEAL);
                 int32 spellid = m_spellInfo->Id;            // send main spell id as basepoints for not used effect
                 m_caster->CastCustomSpell(unitTarget, 19968, &heal, &spellid, nullptr, true);
             }
@@ -3689,6 +3693,8 @@ void Spell::EffectScriptEffect(SpellEffectIndex eff_idx)
                 if (!unitTarget || !unitTarget->isAlive())
                     return;
                 int32 heal = damage;
+                heal = caster->SpellHealingBonusDone(unitTarget, m_spellInfo, heal, HEAL);
+                heal = unitTarget->SpellHealingBonusTaken(caster, m_spellInfo, heal, HEAL);
                 int32 spellid = m_spellInfo->Id;            // send main spell id as basepoints for not used effect
                 m_caster->CastCustomSpell(unitTarget, 19993, &heal, &spellid, nullptr, true);
             }
