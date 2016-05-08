4005dd12cc3a58fda6efc0c80442090d0a0249e6
TarasHots <mgenny954@gmail.com>
2016-05-08 15:34:39 +0300

    Fixed bug when new pet wasn't affected by positive auras immidiately

--- src/game/Pet.cpp
+++ src/game/Pet.cpp
@@ -1032,6 +1032,8 @@ bool Pet::CreateBaseAtCreature(Creature* creature)
         SetUInt32Value(UNIT_MOD_CAST_SPEED, creature->GetUInt32Value(UNIT_MOD_CAST_SPEED));
         SetLoyaltyLevel(REBELLIOUS);
     }
+
+    SetCanModifyStats(true);
     return true;
 }
 
